86247c28b533113419094635ee0c7144c6ed1865
cobelu <cluckett15@austincollege.edu>
2019-03-02 10:24:22 -0600

    setup

---
+++ Lab01_5/Lab01_5/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab01_5
+//
+//  Created by Connor Luckett on 3/2/19.
+//  Copyright © 2019 Connor Luckett. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
