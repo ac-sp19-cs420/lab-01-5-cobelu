ded8f383c0bfc48698b0d18c9039cd4d6ef34f44
cobelu <cluckett15@austincollege.edu>
2019-03-03 19:55:01 -0600

    comment formatting

--- Lab01_5/Lab01_5/stack.h
+++ Lab01_5/Lab01_5/stack.h
@@ -12,9 +12,21 @@
 #include <stdio.h>
 
 /* Create a struct for a stack of integers. This contains one entry for a linked list struct.
+ */
+
+/*
 Create a method push that takes two values and integer and a stack struct. This will push the integer onto the stack.
+ */
+
+/*
 Create a method pop that takes one parameter, the stack struct. This removes the top integer from this list and returns the value
+ */
+
+/*
 Create a method top that takes one parameter, the stack struct. This returns the top integer from this list. It is NOT removed.
+ */
+
+/*
 Create a method is_empty that takes one parameter, the stack struct. This return 1 if the list is empty, and 0 if it contains at least one element.
  */
 
