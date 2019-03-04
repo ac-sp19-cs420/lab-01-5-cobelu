e0ef9ee1839203fcbab3b46cb5666e63d60e23c9
cobelu <cluckett15@austincollege.edu>
2019-03-03 20:00:22 -0600

    stack header

--- Lab01_5/Lab01_5/stack.h
+++ Lab01_5/Lab01_5/stack.h
@@ -10,24 +10,32 @@
 #define stack_h
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /* Create a struct for a stack of integers. This contains one entry for a linked list struct.
  */
+typedef struct Stack {
+    // TODO
+} Stack;
 
 /*
 Create a method push that takes two values and integer and a stack struct. This will push the integer onto the stack.
  */
+int push(Stack stack, int value);
 
 /*
 Create a method pop that takes one parameter, the stack struct. This removes the top integer from this list and returns the value
  */
+int pop(Stack stack);
 
 /*
 Create a method top that takes one parameter, the stack struct. This returns the top integer from this list. It is NOT removed.
  */
+int top(Stack stack);
 
 /*
 Create a method is_empty that takes one parameter, the stack struct. This return 1 if the list is empty, and 0 if it contains at least one element.
  */
+int is_empty(Stack stack);
 
 #endif /* stack_h */
